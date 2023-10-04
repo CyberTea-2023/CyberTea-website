75b9081a2070a7c90d41bef5d881eb00a2701d47
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2023-10-04 10:58:35 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+// import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+
+    const [expand, setExpand] = useState(false)
+
+    const NavToggle = () => {
+        setExpand(!expand)
+    }
+
+    return (
+        <>
+            <nav>
+                <div className="mobview">
+                    <h1>CyberTEA</h1>
+                    <span id='hamburger' className={`material-symbols-outlined ${expand ? `expand` : ``}`} onClick={NavToggle} >
+                        menu
+                    </span>
+                </div>
+                <ul id='nav-right' className={expand ? `expand` : ``}>
+                    {/* <li onClick={() => {
+                        expand ? NavToggle() : ``
+                    }}><Link to={'/'}>Home</Link></li>
+                    <li onClick={() => {
+                        expand ? NavToggle() : ``
+                    }}><Link to={'/projects'}>Projects</Link></li>
+                    <li onClick={() => {
+                        expand ? NavToggle() : ``
+                    }}><Link to={'/contact'}>Contact</Link></li> */}
+                    <li onClick={() => {
+                        expand ? NavToggle() : ``
+                    }}>Home</li>
+                    <li onClick={() => {
+                        expand ? NavToggle() : ``
+                    }}>Projects</li>
+                    <li onClick={() => {
+                        expand ? NavToggle() : ``
+                    }}>Contact</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
