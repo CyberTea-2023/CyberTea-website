2c5d44ca9cde840cf5620529dde163da62bb78fa
Shashank Raj <shashankraj3636@gmail.com>
2023-10-12 01:44:10 +0530

    before finalise

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import { CardSlider } from "./Components/CardSlider/CardSlider"
-import Carousel from "./Components/Carousel/Carousel"
-import Navbar from "./Components/Navbar/Navbar"
-import './App.css'
-import NewsSchedule from "./Components/NewsSchedule/NewsSchedule"
-import FooterComponent from "./Components/FooterComponent/FooterComponent"
-import Registration from "./Components/Registration/Registration"
+import "./App.css";
+import Carousel from "../Components/Carousel/Carousel";
+import Navbar from "../Components/Navbar/Navbar";
+import CardSlider from "../Components/CardSlider/CardSlider";
+import NewsSchedule from "../Components/ScheduleAndRegister/ScheduleAndRegister";
+import FooterComponent from "../Components/FooterComponent/FooterComponent";
+import Registration from "../Components/Registration/Registration";
+import TopHeader from "../Components/Top Header/Header";
+import About from "../Components/About/About";
+import Patron from "../Components/Patron/Patron";
+import TeamHolder from "../Components/Team/TeamHolder";
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <TopHeader />
       <Carousel />
+      <div className="about_container">
+        <About />
+      </div>
+      <Patron />
       <CardSlider />
       <NewsSchedule />
-      <Registration />
+      <TeamHolder />
       <FooterComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
