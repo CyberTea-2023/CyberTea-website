4a36316e3d4e40c945b4097562a18aa0a7052627
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2023-10-13 12:15:55 +0530

    background update

--- Components/Team/TeamHolder.jsx
+++ Components/Team/TeamHolder.jsx
@@ -9,32 +9,32 @@ export default function TeamHolder() {
         <h2 style={{ marginTop: "5px" }}>Lead</h2>
         <div className="team_con">
           <TeamCard
-            name="VryuShab Goyani"
-            link="https://www.iiits.ac.in/home/administration/director/"
-            title="Director, IIIT Sri City, Chittoor"
+            name="Vrushabh Goyani"
+            link="https://www.linkedin.com/in/vrushabh-goyani-843927224"
+            title="IOTA, IIITS"
           />
         </div>
         <h2>Web Dev Team</h2>
         <div className="team_con">
           <TeamCard
-            link="https://www.linkedin.com/in/nikhil-tripathi/"
-            name="Nikhil Tripathi"
-            title="Assistant Professor, IIIT Sri City"
+            link="https://www.linkedin.com/in/sosenkkk"
+            name="Shashank"
+            title="Web Development Lead, IOTA IIITS"
           />
           <TeamCard
-            name="Dr. Neha Agrawal"
-            link="https://www.linkedin.com/in/dr-neha-agrawal-0a32776b/"
-            title="Assistant Professor, IIIT Sri City"
+            link="https://www.linkedin.com/in/mithun-u-b19110266"
+            name="Mithun U"
+            title="Web Development Core, IOTA IIITS"
           />
           <TeamCard
-            link="https://www.linkedin.com/in/nikhil-tripathi/"
-            name="Nikhil Tripathi"
-            title="Assistant Professor, IIIT Sri City"
+            link="https://www.linkedin.com/in/vinayak-anand"
+            name="Vinayak Anand"
+            title="Web Development Lead, IOTA IIITS"
           />
           <TeamCard
-            name="Dr. Neha Agrawal"
-            link="https://www.linkedin.com/in/dr-neha-agrawal-0a32776b/"
-            title="Assistant Professor, IIIT Sri City"
+            name="Charishma Bollineni"
+            link="hhttps://www.linkedin.com/in/charishma-bollineni-46a625283"
+            title="Web Development Lead, IOTA IIITS"
           />
         </div>
         <h2>Design Team</h2>
